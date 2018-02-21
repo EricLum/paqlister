746ddda2e65b816efc7f2cf68e786f84ae33f71f
eal6gf <elum1234@gmail.com>
2018-02-21 16:34:14 -0500

    icons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation'
 import Landing from './containers/Landing'
-import {Row, Col, Card} from 'react-materialize'
+import {Row, Col, Card,Icon} from 'react-materialize'
 
 class App extends Component {
   render() {
@@ -13,13 +13,19 @@ class App extends Component {
         <Landing />
         <Row>
           <Col s={6} s={6} className='grid-example'>
-	          <Card className='blue-grey darken-1' textClassName='white-text' title='Card title'>
-		              Share your items, express yourself!
+	          <Card className='blue-grey darken-4' textClassName='white-text' title='Card title'>
+              <div>
+                <p>Save your collections. Manage your life.</p><br></br>
+                <Icon large={true}>share</Icon>
+              </div>
             </Card>
           </Col>
           <Col s={6} s={6} className='grid-example'>
-            <Card className='grey darken-4' textClassName='white-text' title='Card title'>
-                  Save your collections. Manage your life.
+            <Card className='blue-grey darken-4' textClassName='white-text' title='Card title'>
+                <div>
+                  <p>Save your collections. Manage your life.</p><br></br>
+                  <Icon large={true}>add_box</Icon>
+                </div>
             </Card>
           </Col>
         </Row>
