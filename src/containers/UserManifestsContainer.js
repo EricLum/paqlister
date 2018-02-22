d8c6a2796f3aab026c1ed487f5af84ec70775374
eal6gf <elum1234@gmail.com>
2018-02-22 10:11:24 -0500

    draggable components

--- src/containers/UserManifestsContainer.js
+++ src/containers/UserManifestsContainer.js
@@ -1,16 +1,33 @@
 //Container for showing all of the collections a user has or packs.
 import React from 'react'
 import UserManifestsNavbar from './UserManifestsNavbar'
-
+import Draggable from 'react-draggable'
+import '../styles/UserManifestsContainer.css'
 class UserManifestsContainer extends React.Component {
 
   render(){
+    console.log(this.state)
     return (
       <div>
         <UserManifestsNavbar />
-        <div className=''>
-          test component
-        </div>
+
+        <div className="box" style={{height: '800px', width: '1200px', position: 'relative' , padding: '0'}}>
+           <div style={{height: '750px', width: '1100px', padding: '10px'}}>
+             <Draggable bounds="parent">
+               <div className="box">
+                 I can only be moved within my offsetParent.<br /><br />
+                 Both parent padding and child margin work properly.
+               </div>
+             </Draggable>
+             <Draggable bounds="parent">
+               <div className="box">
+                 I also can only be moved within my offsetParent.<br /><br />
+                 Both parent padding and child margin work properly.
+               </div>
+             </Draggable>
+           </div>
+         </div>
+
       </div>
     )
   }
